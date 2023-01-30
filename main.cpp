52176d159a317491f00de7f02618429f0b9d33cc
Cody McCarty <codyjmccarty@gmail.com>
2023-01-30 17:07:38 -0600

    added win condition

--- main.cpp
+++ main.cpp
@@ -68,6 +68,15 @@ int main()
     scarfyData.frame = 0;
     scarfyData.updateTime = 1.0 / 12.0;
     scarfyData.runningTime = 0.0;
+    Texture2D backgroud = LoadTexture("textures/far-buildings.png");
+    float backgroundScale = viewHeight / 180;
+    float bgX{};
+    Texture2D midground = LoadTexture("textures/back-buildings.png");
+    Texture2D foreground = LoadTexture("textures/foreground.png");
+    float mgx{};
+    float fgx{};
+    float finishLine{nebs[sizeOfNebs - 1].pos.x};
+    bool collision{};
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -75,6 +84,40 @@ int main()
         const float dt{GetFrameTime()};
         BeginDrawing();
         ClearBackground(WHITE);
+
+        bgX -= 20 * dt;
+        if (bgX <= -backgroud.width * backgroundScale)
+        {
+            bgX = 0.0;
+        }
+        Vector2 bg1Pos{bgX, 0.0};
+        DrawTextureEx(backgroud, bg1Pos, 0.0, backgroundScale, WHITE);
+        Vector2 bg2Pos{bgX + backgroud.width * backgroundScale, 0.0};
+        DrawTextureEx(backgroud, bg2Pos, 0.0, backgroundScale, WHITE);
+        Vector2 bg3Pos{bgX + backgroud.width * (backgroundScale * 2), 0.0};
+        DrawTextureEx(backgroud, bg3Pos, 0.0, backgroundScale, WHITE);
+        mgx -= 40 * dt;
+        if (mgx <= -midground.width * backgroundScale)
+        {
+            mgx = 0.0;
+        }
+        Vector2 mg1pos{mgx, 0.0};
+        DrawTextureEx(midground, mg1pos, 0.0, backgroundScale, WHITE);
+        Vector2 mg2pos{mgx + midground.width * backgroundScale, 0.0};
+        DrawTextureEx(midground, mg2pos, 0.0, backgroundScale, WHITE);
+        Vector2 mg3pos{mgx + midground.width * (backgroundScale * 2), 0.0};
+        DrawTextureEx(midground, mg3pos, 0.0, backgroundScale, WHITE);
+        fgx -= 80 * dt;
+        if (fgx <= -foreground.width * backgroundScale)
+        {
+            fgx = 0.0;
+        }
+        Vector2 fg1pos{fgx, 0.0};
+        DrawTextureEx(foreground, fg1pos, 0.0, backgroundScale, WHITE);
+        Vector2 fg2pos{fgx + foreground.width * backgroundScale, 0.0};
+        DrawTextureEx(foreground, fg2pos, 0.0, backgroundScale, WHITE);
+        Vector2 fg3pos{fgx + foreground.width * (backgroundScale * 2), 0.0};
+        DrawTextureEx(foreground, fg3pos, 0.0, backgroundScale, WHITE);
 
         if (isOnGround(scarfyData, viewHeight))
         {
@@ -90,52 +133,56 @@ int main()
         {
             velocity += jumpVel;
         }
-        scarfyData.pos.y += velocity * dt;
         if (!isInAir)
         {
             scarfyData = updateAnimData(scarfyData, dt, 5);
         }
-        // scarfyData.runningTime += dt;
-        // {
-        // if (scarfyData.runningTime >= scarfyData.updateTime)
-        // {
-        //     scarfyData.runningTime = 0.0;
-        //     scarfyData.rec.x = scarfyData.frame * scarfyData.rec.width;
-        //     scarfyData.frame++;
-        //     if (scarfyData.frame > 5)
-        //     {
-        //         scarfyData.frame = 0;
-        //     }
-        // }
-        // }
         for (int i = 0; i < sizeOfNebs; i++)
         {
             nebs[i].pos.x += nebVelocity * dt;
         }
         for (int i = 0; i < sizeOfNebs; i++)
         {
-            nebs[i] = updateAnimData(nebs[i], dt, 8);
-            // nebs[i].runningTime += dt;
-            // if (nebs[i].runningTime >= nebs[i].updateTime)
-            // {
-            //     nebs[i].runningTime = 0.0;
-            //     nebs[i].rec.x = nebs[i].frame * nebs[i].rec.width;
-            //     nebs[i].frame++;
-            //     if (nebs[i].frame > 7)
-            //     {
-            //         nebs[i].frame = 0;
-            //     }
-            // }
+            nebs[i] = updateAnimData(nebs[i], dt, 7);
         }
-        for (int i = 0; i < sizeOfNebs; i++)
+
+        for (AnimData neb : nebs)
         {
-            DrawTextureRec(nebula, nebs[i].rec, nebs[i].pos, WHITE);
+            float padding{50};
+            Rectangle nebRec{neb.pos.x + padding, neb.pos.y + padding / 2, neb.rec.width - padding * 2, neb.rec.height};
+            Rectangle scarfyRec{scarfyData.pos.x, scarfyData.pos.y, scarfyData.rec.width, scarfyData.rec.height};
+            if (CheckCollisionRecs(nebRec, scarfyRec))
+            {
+                collision = true; // TODO: becomes true godmode
+            }
         }
-        DrawTextureRec(scarfy, scarfyData.rec, scarfyData.pos, WHITE);
+
+        if (collision)
+        {
+            DrawText("Game Over!", viewWidth / 2, viewHeight / 2, 20, RED);
+        }
+        else
+        {
+            for (int i = 0; i < sizeOfNebs; i++)
+            {
+                DrawTextureRec(nebula, nebs[i].rec, nebs[i].pos, WHITE);
+            }
+            finishLine += nebVelocity * dt;
+            scarfyData.pos.y += velocity * dt;
+            DrawTextureRec(scarfy, scarfyData.rec, scarfyData.pos, WHITE);
+        }
+
+        if (scarfyData.pos.x > nebs[sizeOfNebs - 1].pos.x + 400)
+        {
+            DrawText("You Win!", viewWidth / 2, viewHeight / 2, 20, RED);
+        }
 
         EndDrawing();
     }
     UnloadTexture(scarfy);
     UnloadTexture(nebula);
+    UnloadTexture(backgroud);
+    UnloadTexture(midground);
+    UnloadTexture(foreground);
     CloseWindow();
 }
